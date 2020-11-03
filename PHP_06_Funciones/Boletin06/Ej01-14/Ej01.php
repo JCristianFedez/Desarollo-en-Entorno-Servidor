ed87640652a5924229ee32ca70e99c5ae6b21abf
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2020-11-03 22:16:59 +0100

    Funcion esCapicua creada y probada

--- PHP_06_Funciones/Boletin06/Ej01-14/Ej01.php
+++ PHP_06_Funciones/Boletin06/Ej01-14/Ej01.php
@@ -1,1 +1,19 @@
--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EsCapicua</title>
+</head>
+<body>
+    <?php 
+    include "misMatematicas.php";
+    $miNum=1221;
+    if(esCapicua($miNum)){
+        echo "$miNum es capicua";
+    }else{
+        echo "$miNum no capicua";
+    }
+    ?>
+</body>
+</html>
--- PHP_06_Funciones/Boletin06/Ej01-14/misMatematicas.php
+++ PHP_06_Funciones/Boletin06/Ej01-14/misMatematicas.php
@@ -1,5 +1,7 @@
 <?php
 
+// Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso
+// en caso contrario
 function esCapicua($num){
     return (strrev($num)==$num)?true:false;
 }
