0738519ecf353bbeba00386ca73171bb371d70e7
JCristianFedez <73657780+JCristianFedez@users.noreply.github.com>
2021-02-04 18:39:46 +0100

    Añadida BAse de datos

---
+++ PHP_12-Modelo_Vista_Controlador/Ejercicios Adicionales/Ej02/DB/ej02-adicional-mvc.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-02-2021 a las 18:38:51
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ej02-adicional-mvc`
+--
+CREATE DATABASE IF NOT EXISTS `ej02-adicional-mvc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `ej02-adicional-mvc`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `matricula` varchar(10) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellidos` varchar(30) NOT NULL,
+  `curso` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`matricula`, `nombre`, `apellidos`, `curso`) VALUES
+('asd111', 'Cristian', 'Fernandez Tirado', '2DAW'),
+('asd112', 'Antoino', 'Martinez Gutierez', '2ESO'),
+('asd113', 'David', 'Saeta Gutierez', '1DAW'),
+('asd114', 'Pedro', 'Garcia Garcia', '2ESO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno-asignatura`
+--
+
+CREATE TABLE `alumno-asignatura` (
+  `matriculaAlumno` varchar(10) NOT NULL,
+  `idAsignatura` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignatura`
+--
+
+CREATE TABLE `asignatura` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `asignatura`
+--
+
+INSERT INTO `asignatura` (`id`, `nombre`) VALUES
+(1, 'DWEC'),
+(3, 'DWES'),
+(4, 'Ingles'),
+(5, 'EIE'),
+(6, 'DAW'),
+(7, 'DIW'),
+(8, 'Matematicas'),
+(9, 'Fisica'),
+(10, 'EF'),
+(11, 'Plastica');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`matricula`);
+
+--
+-- Indices de la tabla `alumno-asignatura`
+--
+ALTER TABLE `alumno-asignatura`
+  ADD PRIMARY KEY (`matriculaAlumno`,`idAsignatura`),
+  ADD KEY `idAsignatura` (`idAsignatura`);
+
+--
+-- Indices de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumno-asignatura`
+--
+ALTER TABLE `alumno-asignatura`
+  ADD CONSTRAINT `alumno-asignatura_ibfk_1` FOREIGN KEY (`matriculaAlumno`) REFERENCES `alumno` (`matricula`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `alumno-asignatura_ibfk_2` FOREIGN KEY (`idAsignatura`) REFERENCES `asignatura` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
