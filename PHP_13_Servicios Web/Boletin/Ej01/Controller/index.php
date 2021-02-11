d2d3994118a71ce064dc8b5a11ddb00bc4171530
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2021-02-11 13:46:35 +0100

    Ej01 Servicios php terminado

--- PHP_13_Servicios Web/Boletin/Ej01/Controller/index.php
+++ PHP_13_Servicios Web/Boletin/Ej01/Controller/index.php
@@ -1,8 +1,40 @@
 <?php 
+if(session_status() == PHP_SESSION_NONE){
+    session_start();
+}
 
-$datos = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Malaga,Spain&units=metric&appid=3beff7fc37c44498c438468e595cc369");
+$data["provincia"] = "";
+$data["icon"] = "";
+$data["tiempo"] = "";
 
-$tiempo = json_decode($datos);
+// Lo siguiente es para que cuando recarges la pagina no se renvie la peticion
+if(!isset($_REQUEST["provincia"]) && isset($_SESSION["provincia"])){
+    $data["provincia"] = $_SESSION["provincia"];
+    unset($_SESSION["provincia"]);
+
+    $datos = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$data["provincia"].",Spain&units=metric&appid=3beff7fc37c44498c438468e595cc369");
+    
+    // Si la provincia existe
+    if($datos){
+        $tiempo = json_decode($datos);
+
+        $data["icon"] = "http://openweathermap.org/img/w/".$tiempo->weather[0]->icon.".png";
+        $data["tiempo"] = "<br>Tiempo: " . $tiempo->weather[0]->description . "<br>";
+        $data["tiempo"] .= "Temperatura: " . $tiempo->main->temp . "<br>";
+        $data["tiempo"] .= "Temp Max: " . $tiempo->main->temp_max . "<br>";
+        $data["tiempo"] .= "Temp Min: " . $tiempo->main->temp_min . "<br>";
+        $data["tiempo"] .= "Sensacion termica: " . $tiempo->main->feels_like . "<br>";
+
+    }else{
+        $data["provincia"] = "";
+    }
+    
+// Cuando se pinche en una provincia se guardara en una session y se recargara la pagina
+}else if(isset($_REQUEST["provincia"])){
+    $_SESSION["provincia"] = $_REQUEST["provincia"];
+    header("Location: index.php");
+
+}
 
 include "../View/paginaPrincipal.php"
 ?>
